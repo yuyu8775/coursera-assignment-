bf538736834555268b91bce4395fc53081b90f74
yuyu8775 <64871818+yuyu8775@users.noreply.github.com>
2020-05-05 15:19:10 -0700

    capstone project assignment 1

---
+++ capstone project.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### this notebook will be mainly used for the capstone project. ###
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
